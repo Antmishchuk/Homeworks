9a556c848c53a778fe1916d6c3388d8bd5c39fd3
Antmishchuk <168056473+Antmishchuk@users.noreply.github.com>
2024-05-20 07:44:32 +0400

    Add files via upload

---
+++ For.py
@@ -0,0 +1,6 @@
+cars_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(cars_list)):
+    cars_count += 10
+    print('Я езжу на автомобиле ' + cars_list[i])
+    print(cars_count)
