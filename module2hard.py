0a79e4037bd072a6cf06321f834f4d84aa9f3c52
Antmishchuk <168056473+Antmishchuk@users.noreply.github.com>
2024-05-22 06:04:47 +0400

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -1,10 +1,10 @@
 my_input = int(input('Введите число: '))
-pare = []
+pare = set()
 for i in range(1, my_input):
     for j in range(i+1, my_input):
         a = i + j
         if  my_input % a == 0:
-            pare.append(f'{i}{j}')
+            pare.add(f'{i}{j}')
             result = (''.join(pare))
 print(f'{my_input} - {result}')
 
