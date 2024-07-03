9c683cdff7d3e41da33ee3e6d0892e95ea8c33e4
Antmishchuk <168056473+Antmishchuk@users.noreply.github.com>
2024-07-03 11:01:34 +0400

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,10 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if root_word.lower() in str(word).lower():
+            same_words.append(word)
+        elif str(word).lower() in root_word.lower():
+            same_words.append(word)
+    print(*same_words)
+single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
