653f9ae79a00d26a1beb92c5f72ff6c8578fe6fe
Antmishchuk <168056473+Antmishchuk@users.noreply.github.com>
2024-07-23 11:59:46 +0400

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+def divide(first, second):
+    if second != 0:
+        res = first / second
+        return print(res)
+    else:
+        return print(inf)
+# divide(49, 7)
+# divide(15, 0)
