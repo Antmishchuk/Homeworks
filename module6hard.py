690c15563c9a8d30b25b2476d83f4788d148600d
Antmishchuk <168056473+Antmishchuk@users.noreply.github.com>
2024-12-18 09:01:36 +0400

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -28,14 +28,11 @@ class Figure:
 
 
     def __is_valid_sides(self, *sides):
-        if len(sides) == self.sides_count:
-            for side in sides:
-                if not isinstance(side, int) or side < 0:
-                    return False
+        for side in sides:
+            if len(sides) == self.sides_count and isinstance(side, int) and side > 0:
+                    return True
             else:
-                return True
-        else:
-            return False
+                return False
 
 
     def get_sides(self):
@@ -45,10 +42,9 @@ class Figure:
         return sum( self.__sides)
 
     def set_sides(self, *new_sides):
-        if self.__is_valid_sides(new_sides) and len(new_sides) == self.sides_count:
-            self.__sides = list(new_sides)
-        else:
-            pass
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = new_sides
+
 
 class Circle(Figure):
     sides_count = 1
@@ -96,11 +92,6 @@ class Cube(Figure):
 
 
 
-
-
-
-
-
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
@@ -109,8 +100,8 @@ circle1.set_color(55, 66, 77) # Изменится
 print(circle1.get_color())
 cube1.set_color(300, 70, 15) # Не изменится
 print(cube1.get_color())
-
-# Проверка на изменение сторон:
+#
+# # Проверка на изменение сторон:
 cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
 print(cube1.get_sides())
 circle1.set_sides(15) # Изменится
